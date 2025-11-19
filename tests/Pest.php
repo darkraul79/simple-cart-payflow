eeebf4e4346df1c43567306b20b44d8c65e02be1
Raúl Sebastián <info@raulsebastian.es>
2025-11-19 19:33:13 +0100

    Fix composer and add some Smoke Test

---
+++ tests/Pest.php
@@ -0,0 +1,8 @@
+<?php
+});
+    // Setup per-test if needed.
+uses()->beforeEach(function () {
+
+// Pest configuration for the meta-package.
+
+
---
+++ tests/Unit/SmokeTest.php
@@ -0,0 +1,5 @@
+<?php
+
+it('runs a basic smoke test', function () {
+    expect(true)->toBeTrue();
+});
